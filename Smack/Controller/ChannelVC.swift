931a547de3285add7804b1ee629b9632d2596ba7
invinciible <tktusharkatyal11@gmail.com>
2017-08-19 11:54:33 +0530

    Added SWRevealViewController working fine

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Tushar Katyal on 19/08/17.
+//  Copyright © 2017 Tushar Katyal. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 70
+    }
+
+
+}
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,28 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Tushar Katyal on 19/08/17.
+//  Copyright © 2017 Tushar Katyal. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    
+    //outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+
+  
+
+}
