6ea64653057d02ef7f41c9388a372634082b5608
invinciible <tktusharkatyal11@gmail.com>
2017-08-23 02:35:54 +0530

    timeStamp working

--- Smack/View/MessageCell.swift
+++ Smack/View/MessageCell.swift
@@ -29,6 +29,20 @@ class MessageCell: UITableViewCell {
         userImg.image = UIImage(named: message.userAvatar)
         userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
         
+        guard var isoDate = message.timeStamp else {return}
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d , h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLbl.text = finalDate
+        }
     }
 
 }
